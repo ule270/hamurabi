2d1eae7225a2dcefc0a081ce221cee1b26110d37
ule270 <u.le270@gmail.com>
2023-03-19 11:28:52 -0400

    pom.xml added

---
+++ Hammurabi.java
@@ -0,0 +1,19 @@
+package hammurabi;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Hammurabi {
+    Random rand = new Random();
+    Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        new Hammurabi().playGame();
+    }
+
+    void playGame() {
+        // declare local variables here: grain, population, etc.
+        // statements go after the declarations
+    }
+
+    //other methods go here
+}
